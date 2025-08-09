91ddb64fb3b7128b430712a5487d2e84eb13980a
arebhysri <arebhysri@gmail.com>
2025-08-09 12:31:13 +1200

    updated code

--- guessWordGame.py
+++ guessWordGame.py
@@ -11,23 +11,27 @@ guessword = ['_'] * len(word)
 #initialize how many attempt the user can do
 attempt = 10
 
-#statr the loopa
-while attempt>0:
-    print('\nCurrent word: ' + ' '.join(guessword))
-    guess = input('Guess a letter: ').lower()
+def game_Start(attempt):
+#start the loop
+    while attempt>0:
+        print('\nCurrent word: ' + ' '.join(guessword))
+        guess = input('Guess a letter: ').lower()
 
-    if guess in word:
-        for i in range(len(word)):
-            if word[i] == guess:
-                guessword[i]=guess
-        print('found the letter')
-    else:
-        attempt -=1
-        print(f'Lossed on attempt ! you have {attempt} left')
+        if guess in word:
+            for i in range(len(word)):
+                if word[i] == guess:
+                    guessword[i]=guess
+            print('found the letter')
+        else:
+            attempt -=1
+            print(f'Lost on attempt ! you have {attempt} left')
 
-    if '_' not in guessword:
-        print('You have found the word ' +word)
-        break
+        if '_' not in guessword:
+            print('You have found the word ' +word)
+            break
 
-if attempt == 0 and '_' in guessword:
-    print('you have lost... the word is ' + word)
+    if attempt == 0 and '_' in guessword:
+        print('you have lost... the word is ' + word)
+
+if __name__ == "__main__":
+    game_Start(10)
