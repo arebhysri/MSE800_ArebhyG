70e51aa4cb16fee9c95764391e904df173d174c9
arebhysri <arebhysri@gmail.com>
2025-08-10 08:44:57 +1200

    task3 of week2

---
+++ mtdTask3.py
@@ -0,0 +1,30 @@
+class StringManipulator:
+    def __init__(self, text):
+        self.text = text
+
+    def find_character(self, char):
+        return self.text.find(char)
+    
+    def find_length(self):
+        return len(self.text)
+    
+    def make_uppercase(self):
+        return self.text.upper()
+    
+def main():
+    #Create an instance of the StringManipulator class
+    name = StringManipulator("assignment")
+
+    #Call the find_character method on the object
+    result = name.find_character('g')
+    print("g is placed on" , result) #output 1
+
+    length = name.find_length()
+    print("Length of the word is " ,length)
+
+    uppercase = name.make_uppercase()
+    print("Print all as Upper case " ,uppercase)
+
+if __name__ == "__main__":
+    main()
+
