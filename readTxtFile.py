7bdf30599150b44b360baa6fe684e2c65302ef26
arebhysri <arebhysri@gmail.com>
2025-08-16 12:28:24 +1200

    updated for task 2

--- readTxtFile.py
+++ readTxtFile.py
@@ -15,10 +15,20 @@ class readFile:
             file.write("Hello there")
             print("File content has been written to : " ,self.outputFile)
 
+    def countTheWords(self):
+        with open(self.inputFile, "r", encoding="UTF-8") as countFile:
+            wordcount = 0
+            for line in countFile:
+                lin = line.rstrip()
+                wds = lin.split()
+                wordcount += len(wds)
+        print("Total number of words:", wordcount)
+
 def main():
     fileManager = readFile("demo.txt","output.txt")
     fileManager.readingFile()
     fileManager.writingFile()
+    fileManager.countTheWords()
 
 if __name__ == "__main__":
     main()
