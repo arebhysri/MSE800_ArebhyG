93523ef82d155e81d0b90d8f60fe62de9304d7d0
arebhysri <arebhysri@gmail.com>
2025-10-18 11:37:32 +1300

    task 1

--- Week12/flask_front_end.py
+++ Week12/flask_front_end.py
@@ -4,3 +4,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_flask():
     return "<p>Hello, Flask!</p>"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"<p>Hello, {name}!</p>"
+
+@app.route('/bye')
+def bye():
+    return "<p>Goodbye!</p>"
+
+@app.route('/<name>/<int:number>')
+def repeat_name(name, number):
+    return f"<p>Hi I'm {name} I'm {number} years old.</p>"
